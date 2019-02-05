881d600c51290aadf620f17e9a686131d91e97a9
briangacheru <briangacheru14@gmail.com>
2019-02-05 17:07:09 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset = "UTF-8" name "viewport" content = "width-device=width, initial-scale=1"/>
+		<title>OOP PHP Registrarion and Login Form Using MySQLi</title>
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css" />
+	</head>
+<body>
+	<nav class = "navbar navbar-default">
+		<div class = "container-fluid">
+			<a class = "navbar-brand" href = "https://www.sourcecodester.com">Sourcecodester</a>
+		</div>
+	</nav>
+	<br />
+	<br />
+	<br />
+	<div class = "row">
+		<div class = "col-md-4">
+		</div>
+		<div class = "col-md-4 well">
+			<h4 class = "text-danger">OOP PHP Registration and Login Form Using MySQLi</h4>
+			<hr style = "border-top:1px dotted #000;"/>
+			<form method = "POST" action = "save_query.php">
+				<div class="form-group">
+					<input type = "text" placeholder = "Username"  name = "username" class = "form-control" required = "required"/>
+				</div>
+				<div class="form-group">
+					<input type = "password" placeholder = "Password"  name = "password" class = "form-control" required = "required">
+				</div>
+				<div class="form-group">
+					<input type = "text" placeholder = "Firstname"  name = "firstname" class = "form-control" required = "required"/>
+				</div>
+				<div class="form-group">
+					<input type = "text" placeholder = "Lastname"  name = "lastname" class = "form-control" required = "required"/>
+				</div>
+				<button class = "btn btn-success puul-left" name = "signup"><span class = "glyphicon glyphicon-edit"></span> Sign up</button>
+				<label class = "pull-right">Already have an account? <a href = "login.php"> Click here</a></label>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta charset = "UTF-8" name "viewport" content = "width-device=width, initial-scale=1"/>
+		<title>OOP PHP Registrarion and Login Form Using MySQLi</title>
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css" />
+	</head>
+<body>
+	<nav class = "navbar navbar-default">
+		<div class = "container-fluid">
+			<a class = "navbar-brand" href = "https://www.sourcecodester.com">Sourcecodester</a>
+		</div>
+	</nav>
+	<br />
+	<br />
+	<br />
+	<div class = "row">
+		<div class = "col-md-4">
+		</div>
+		<div class = "col-md-4 well">
+			<h4 class = "text-danger">OOP PHP Registration and Login Form Using MySQLi</h4>
+			<hr style = "border-top:1px dotted #000;"/>
+			<form method = "POST" action = "login_query.php">
+				<div class="form-group">
+					<input type = "text" placeholder = "Username"  name = "username" class = "form-control" required = "required"/>
+				</div>
+				<div class="form-group">
+					<input type = "password" placeholder = "Password"  name = "password" class = "form-control" required = "required">
+				</div>
+				<button class = "btn btn-primary pull-left" name = "login"><span class = "glyphicon glyphicon-log-in"></span> Login</button>
+				<label class = "pull-right">Don't have an account yet? <a href = "index.php"> Click here</a></label>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
